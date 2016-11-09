07c38e535e468d2d1e46d684a0717b82279c648a
Jeffrey Chang <jeffreycj.chang@gmail.com>
2016-11-09 18:07:28 +0000

    Generate a random string as the short url and saves it to database

--- express_server.js
+++ express_server.js
@@ -1,3 +1,4 @@
+'use strict';
 var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
@@ -5,6 +6,17 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
+
+const generateRandomString = function (){
+  const alphaNumeric = "01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  let password = "";
+  let randomNum;
+  for (let i = 0; i <= 6; i++){
+    randomNum = Math.floor((Math.random() * alphaNumeric.length) + 1);
+    password += alphaNumeric.charAt(randomNum);
+  };
+  return password;
+}
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -22,6 +34,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 // THIS ONE WILL DISPLAY A SINGLE URL AND ITS SHORTENED FORM
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id,
@@ -29,13 +45,20 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+// THIS WILL GET THE LONG URL FROM THE USER AND THEN SEND THEM TO THE
+// SHORT URL PAGE THAT DISPLAYS THEIR LONG AND SHORT URLS
+app.post("/urls", (req, res) => {
+  var shortURL = generateRandomString();
+  urlDatabase[shortURL] = req['body']['longURL'];
+  // console.log(req.body);  // debug statement to see POST parameters
+  console.log(urlDatabase)
+  // Respond with 'Ok' (we will replace this)
+  res.send(`Redirecting you to http://localhost:8080/urls/${shortURL}`);
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body);  // debug statement to see POST parameters
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+app.get("/urls/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL);
 });
 
 app.get("/hello", (req, res) => {
