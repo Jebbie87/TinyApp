aa7f626a299cfd2cbfee58c279f9f9ba8fd3a489
Jeffrey Chang <jeffreycj.chang@gmail.com>
2016-11-12 23:01:25 +0000

    Added some user specific functionality

--- express_server.js
+++ express_server.js
@@ -13,7 +13,7 @@ const generateRandomString = function (){
   const alphaNumeric = '01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
   let password = '';
   let randomNum;
-  for (let i = 0; i <= 6; i++){
+  for (let i = 0; i < 6; i++){
     randomNum = Math.floor((Math.random() * alphaNumeric.length) + 1);
     password += alphaNumeric.charAt(randomNum);
   };
@@ -22,10 +22,15 @@ const generateRandomString = function (){
 
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
-  '9sm5xK': 'http://www.google.com'
+  '9sm5xK': 'http://www.google.com',
+  'asdfasfd': 'http://reddit.com'
 };
 
-const users = {};
+const users = {'asdf': {'id': 'asdf',
+                        'email': 'test@test.com',
+                        'password': 'asdf',
+                        'urls': {'b2xVn2': 'http://www.lighthouselabs.ca',
+                                 '9sm5xK': 'http://www.google.com'} }};
 
 app.get('/', (req, res) => {
   res.redirect('/');
@@ -33,13 +38,32 @@ app.get('/', (req, res) => {
 
 // THIS WILL PRINT OUT ALL THE URLS AND THE SHORTENED FORMS
 app.get('/urls', (req, res) => {
-  let cookieID = req['cookies']['user_id']
+    // let cookieID = req['cookies']['user_id']
+  const url = [];
+  let user = req.cookies['user_id'];
+  if(!user){
+    // user = '';
+    let templateVars = {
+    urls: urlDatabase,
+    user: '',
+    email: '',
+    users: users
+  };
+    res.render('urls_index', templateVars);
+  } else if (!users[user]['urls']){
+    res.redirect('/urls/new')
+  } else {
+
   let templateVars = {
     urls: urlDatabase,
-    user: req.cookies['user_id'],
-    email: users[cookieID]['email']
+    user: user,
+    email: users[user]['email'],
+    users: users
   };
+  url.push(req['body']['longURL'])
+  users[['user_id']['urls']] = url
   res.render('urls_index', templateVars);
+  }
 });
 
 // THIS WILL SEND THE USER TO MAKE THE NEW WEBPAGE
@@ -55,11 +79,12 @@ app.post('/urls/:shortURL/delete', (req, res) => {
 
 // THIS ONE WILL DISPLAY A SINGLE URL AND ITS SHORTENED FORM
 app.get('/urls/:id', (req, res) => {
-  let cookieID = req.cookies['user_id']
-  let templateVars = { shortURL: req.params.id,
-                       longURL: urlDatabase[req.params.id],
-                       user: req.cookies['user_id'],
-                       email: users[cookieID]['email']};
+  let cookieID = req['cookies']['user_id']
+  let templateVars = { shortURL: req['params']['id'],
+                       longURL: urlDatabase[req['params']['id']],
+                       user: req['cookies']['user_id'],
+                       email: users[cookieID]['email']
+                    };
   res.render('urls_show', templateVars);
 });
 
@@ -72,34 +97,49 @@ app.get('/u/:shortURL', (req, res) => {
 // THIS WILL GET THE LONG URL FROM THE USER AND THEN SEND THEM TO THE
 // SHORT URL PAGE THAT DISPLAYS THEIR LONG AND SHORT URLS
 app.post('/urls', (req, res) => {
+  const longURL = req['body']['longURL']
+  const cookieID = req['cookies']['user_id']
+  let userURL = users[req['cookies']['user_id']]
   let shortURL = generateRandomString();
-  urlDatabase[shortURL] = req['body']['longURL'];
+  urlDatabase[shortURL] = longURL;
+  if (!userURL.hasOwnProperty('urls')){
+    userURL['urls']= {};
+    userURL['urls'][shortURL] = longURL
+  } else
+  userURL['urls'][shortURL] = longURL
   res.redirect(`urls/${shortURL}`);
 });
 
 // THIS WILL EDIT THE LONG URL
 app.post('/urls/:shortURL', (req, res) => {
+  const shortURL = req['params']['shortURL']
+  const longURL = req['body']['longURL']
   urlDatabase[req['params']['shortURL']] = req['body']['longURL'];
+  urlDatabase[req['cookies']['user_id']]
   res.redirect('/urls');
+});
+
+// THIS WILL
+app.post('/login', (req, res) => {
+  let exists = false;
+  let cookieUser;
+  Object.keys(users).forEach(function(user){
+    if (users[user]['email'] == req['body']['login'] && users[user]['password'] == req['body']['password']){
+      exists = true;
+      cookieUser = users[user]['id']
+    }
+  })
+  if (exists === true){
+    res.cookie('user_id', cookieUser)
+    res.redirect('/urls')
+  } else {
+    res.redirect('/urls/9sm5xK')
+  }
 });
 
 app.get('/login', (req, res) => {
   res.render('urls_login')
 })
-
-// THIS WILL
-app.post('/login', (req, res) => {
-  Object.keys(users).forEach(function(user){
-    if(users[user]['email'] === req['body']['login']) {
-      res.cookie('user_id', users[user]['id'])
-      res.redirect('/urls');
-      return
-    } else {
-      res.redirect('/login')
-      return
-    }
-  })
-});
 
 app.post('/logout', (req, res) => {
   res.clearCookie('user_id');
@@ -110,41 +150,31 @@ app.get('/register', (req, res) => {
   res.render('urls_register');
 });
 
-
-
 app.post('/register', (req, res) => {
-  // console.log(req['body']['email'])
-  // console.log(req['body'])
-
-// if (req['body']['email'] === '' || req['body']['password'] === '') {
-//     res.redirect(400, '/urls/9sm5xK');
-//     return
-//   Object.keys(users).forEach(function(user){
-//     if (users[user]['email'] === req['body']['email']){
-//       res.redirect(400, '/urls/b2xVn2');
-//       return
-//     };
-//   });
-
-//   } else {
-  let userID = generateRandomString();
-  users[userID] = {
+  let foundExisting = false;
+  if (req['body']['email'] === '' || req['body']['password'] === ''){
+      res.redirect(400, '/urls/9sm5xK');
+      return
+  } else {
+    Object.keys(users).forEach(function(user){
+      if (req['body']['email'] === users[user]['email']){
+        foundExisting = true;
+        // res.redirect(400, '/urls/b2xVn2')
+      }
+    })
+  }
+  if (foundExisting === false){
+    let userID = generateRandomString();
+    users[userID] = {
       'id': userID,
       'email': req['body']['email'],
       'password': req['body']['password']
-  };
-  if (req['body']['email'] === '' || req['body']['password'] === ''){
-      res.redirect(400, '/urls/9sm5xK');
+    };
+    res.cookie('user_id', userID);
+    res.redirect('/urls');
+  } else {
+    res.redirect(400, '/urls/b2xVn2')
   }
-
-  Object.keys(users).forEach(function(user){
-    console.log(user)
-  })
-  // console.log(users)
-  res.cookie('user_id', userID);
-  res.redirect('/urls');
-  return
-  // };
 });
 
 
