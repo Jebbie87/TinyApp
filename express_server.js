4bd03e82aa04e6349801d1bfe194e81a379888ad
Jeffrey Chang <jeffreycj.chang@gmail.com>
2016-11-13 18:52:40 +0000

    Wiped entire code and started everything from new

--- express_server.js
+++ express_server.js
@@ -33,156 +33,57 @@ const users = {'asdf': {'id': 'asdf',
                                  '9sm5xK': 'http://www.google.com'} }};
 
 app.get('/', (req, res) => {
-  res.redirect('/');
-});
-
-// THIS WILL PRINT OUT ALL THE URLS AND THE SHORTENED FORMS
-app.get('/urls', (req, res) => {
-    // let cookieID = req['cookies']['user_id']
-  const url = [];
-  let user = req.cookies['user_id'];
-  if(!user){
-    // user = '';
-    let templateVars = {
-    urls: urlDatabase,
-    user: '',
-    email: '',
-    users: users
-  };
-    res.render('urls_index', templateVars);
-  } else if (!users[user]['urls']){
-    res.redirect('/urls/new')
-  } else {
-
-  let templateVars = {
-    urls: urlDatabase,
-    user: user,
-    email: users[user]['email'],
-    users: users
-  };
-  url.push(req['body']['longURL'])
-  users[['user_id']['urls']] = url
-  res.render('urls_index', templateVars);
-  }
-});
-
-// THIS WILL SEND THE USER TO MAKE THE NEW WEBPAGE
-app.get('/urls/new', (req, res) => {
-  res.render('urls_new');
-});
-
-// THIS WILL DELETE THE URL OFF THE DATABASE WHEN DELETE IS CLICKED
-app.post('/urls/:shortURL/delete', (req, res) => {
-  delete urlDatabase[req['params']['shortURL']];
-  res.redirect('/urls');
-});
-
-// THIS ONE WILL DISPLAY A SINGLE URL AND ITS SHORTENED FORM
-app.get('/urls/:id', (req, res) => {
-  let cookieID = req['cookies']['user_id']
-  let templateVars = { shortURL: req['params']['id'],
-                       longURL: urlDatabase[req['params']['id']],
-                       user: req['cookies']['user_id'],
-                       email: users[cookieID]['email']
-                    };
-  res.render('urls_show', templateVars);
-});
-
-// THIS WILL REDIRECT YOU TO THE WEBSITE
-app.get('/u/:shortURL', (req, res) => {
-  let longURL = urlDatabase[req['params']['shortURL']];
-  res.render('urls_new');
-});
-
-// THIS WILL GET THE LONG URL FROM THE USER AND THEN SEND THEM TO THE
-// SHORT URL PAGE THAT DISPLAYS THEIR LONG AND SHORT URLS
-app.post('/urls', (req, res) => {
-  const longURL = req['body']['longURL']
-  const cookieID = req['cookies']['user_id']
-  let userURL = users[req['cookies']['user_id']]
-  let shortURL = generateRandomString();
-  urlDatabase[shortURL] = longURL;
-  if (!userURL.hasOwnProperty('urls')){
-    userURL['urls']= {};
-    userURL['urls'][shortURL] = longURL
-  } else
-  userURL['urls'][shortURL] = longURL
-  res.redirect(`urls/${shortURL}`);
-});
-
-// THIS WILL EDIT THE LONG URL
-app.post('/urls/:shortURL', (req, res) => {
-  const shortURL = req['params']['shortURL']
-  const longURL = req['body']['longURL']
-  urlDatabase[req['params']['shortURL']] = req['body']['longURL'];
-  urlDatabase[req['cookies']['user_id']]
-  res.redirect('/urls');
-});
-
-// THIS WILL
-app.post('/login', (req, res) => {
-  let exists = false;
-  let cookieUser;
-  Object.keys(users).forEach(function(user){
-    if (users[user]['email'] == req['body']['login'] && users[user]['password'] == req['body']['password']){
-      exists = true;
-      cookieUser = users[user]['id']
-    }
-  })
-  if (exists === true){
-    res.cookie('user_id', cookieUser)
-    res.redirect('/urls')
-  } else {
-    res.redirect('/urls/9sm5xK')
-  }
-});
-
-app.get('/login', (req, res) => {
-  res.render('urls_login')
-})
-
-app.post('/logout', (req, res) => {
-  res.clearCookie('user_id');
-  res.redirect('/login');
-});
-
-app.get('/register', (req, res) => {
-  res.render('urls_register');
-});
-
-app.post('/register', (req, res) => {
-  let foundExisting = false;
-  if (req['body']['email'] === '' || req['body']['password'] === ''){
-      res.redirect(400, '/urls/9sm5xK');
-      return
-  } else {
-    Object.keys(users).forEach(function(user){
-      if (req['body']['email'] === users[user]['email']){
-        foundExisting = true;
-        // res.redirect(400, '/urls/b2xVn2')
-      }
-    })
-  }
-  if (foundExisting === false){
-    let userID = generateRandomString();
-    users[userID] = {
-      'id': userID,
-      'email': req['body']['email'],
-      'password': req['body']['password']
-    };
-    res.cookie('user_id', userID);
+  const user = 'asdf' //req['cookies']['user_id'];
+  if (!user) {
     res.redirect('/urls');
   } else {
-    res.redirect(400, '/urls/b2xVn2')
-  }
+    res.redirect('/login');
+  };
 });
 
+app.get('/urls', (req, res) => {
+  const user = 'asdf' //req['cookies']['user_id'];
+  let templateVars = { shortURL: users[user]['urls'],
+                       longURL: users[user]['urls']
+                    };
+  if (!user) {
+    res.status(400);
+    res.render('error-page');
+  } else {
+    res.status(400);
+    res.render('urls_index');
+  };
+});
 
+app.get('/urls/new', (req, res) => {
+  const user = 'asdf' // req.cookies['user_id'];
+  if(!user) {
+    res.status(401);
+    res.render('error-page');
+  } else {
+    res.status(200);
+    res.render('urls_new');
+  };
+});
 
+app.get('/urls/:id', (req, res) => {
+  const user = 'asdf' //
+  if(!urlDatabase.hasOwnProperty(req['params']['id'])) {
+    res.response(404).send('Sorry this page does not exist');
+  } else if (!user) {
+    res.response(401);
+  }
+})
 
-
-
-
+app.get('/u/:id', (req, res) => {
+  const longURL = urlDatabase[req['params']['id']];
+  if (!urlDatabase.hasOwnProperty(req['params']['id'])) {
+    res.status(404);
+    res.render('404-error');
+  } else {
+    res.redirect(longURL)
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
